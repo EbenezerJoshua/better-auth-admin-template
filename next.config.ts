7a358349fcab3a4e7b0196167c27d0d03059a35d
Ebenezer Joshua <ebenezerjoshua1410@gmail.com>
2026-03-09 12:05:48 +0530

    added skills, added badge component ad then changed the localtion of utils into lib folder

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,30 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/u/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        pathname: "/avatars/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      // {
+      //   protocol: "https",
+      //   hostname: "img.clerk.com",
+      //   pathname: "/**",
+      // },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/ui/password-input.tsx
+++ src/components/ui/password-input.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { cn } from "@/lib/auth/utils"
+import { cn } from "@/lib/utils"
 import { EyeIcon, EyeOffIcon } from "lucide-react"
 import { useState, type ComponentProps } from "react"
 
