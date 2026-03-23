8ce06f226eb0d4245aaa4b37e126d06105e1cf6b
Ebenezer Joshua <ebenezerjoshua1410@gmail.com>
2026-03-24 00:47:20 +0530

    updated drizzle setup to use pool and local postgres drivers

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,21 +1,10 @@
-import { config } from "dotenv";
+import 'dotenv/config';
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema  from "./schema";
+import * as schema from "./schema";
 
-config({ path: ".env" });
-
-// Use this for local PG database.
-// export const db = drizzle(process.env.DATABASE_URL!, {schema}); 
-
-// This is for the supabase
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL!,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+  connectionString: process.env.DATABASE_URL!,
 });
 
-export const db = drizzle(pool, { schema })
-
-console.log(process.env.DATABASE_URL)
+export const db = drizzle(pool, { schema });
